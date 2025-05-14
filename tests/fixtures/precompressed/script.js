58a4f96da7beec058b4e969207c8f9e777b443f6
Molly Rose <molly@mollymorphous.dev>
2025-05-14 18:27:11 +0000

    Add precompressed static file test

---
+++ tests/fixtures/precompressed/script.js
@@ -0,0 +1,5 @@
+// Repeat text so there's something to compress
+console.log("Line 1");
+console.log("Line 2");
+console.log("Line 3");
+console.log("Line 4");
--- tests/test_static_files.py
+++ tests/test_static_files.py
@@ -44,3 +44,30 @@ async def test_static_files_approute(server_static_files, runtime_mode):
         res = httpx.get(f'http://localhost:{port}/info')
 
     assert res.status_code == 200
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('server_static_files', ['asgi', 'rsgi', 'wsgi'], indirect=True)
+@pytest.mark.parametrize('runtime_mode', ['mt', 'st'])
+@pytest.mark.parametrize(
+    'encoding',
+    [
+        ('identity', None, 140),
+        ('gzip', 'gzip', 110),
+        ('gzip, zstd', 'zst', 91),
+        ('gzip, zstd, br', 'br', 70),
+    ],
+)
+async def test_static_files_precompressed(server_static_files, runtime_mode, encoding):
+    accept_encoding, content_encoding, body_length = encoding
+
+    async with server_static_files(runtime_mode) as port:
+        res = httpx.get(
+            f'http://localhost:{port}/static/precompressed/script.js',
+            headers={'accept-encoding': accept_encoding},
+        )
+
+    assert res.status_code == 200
+    assert res.headers.get('content-type') == 'text/javascript'
+    assert res.headers.get('content-encoding') == content_encoding
+    assert len(await res.aread()) == body_length
